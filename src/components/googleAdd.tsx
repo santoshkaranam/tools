f0e1dc3d6c9658b68b8c633f093aa483c9a09c6b
santoshkaranam <karanam@iiasa.ac.at>
2023-07-11 07:38:32 +0200

    fix error

--- src/components/googleAdd.tsx
+++ src/components/googleAdd.tsx
@@ -10,19 +10,17 @@ export default function GoogleAdd() {
     return null;
   }
   return (
-    <>
-      <div>
-        <h3 className="pb-4 mb-4 "> Adsense ads </h3>
-        <ins
-          className="adsbygoogle"
-          style={{ display: 'block' }}
-          data-ad-client="ca-pub-1482811726476217"
-          data-ad-slot="776****95"
-          data-ad-format="auto"
-          data-full-width-responsive="true"
-        />
-        <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
-      </div>
-    </>
+    <div>
+      <h3 className="pb-4 mb-4 "> Adsense ads </h3>
+      <ins
+        className="adsbygoogle"
+        style={{ display: 'block' }}
+        data-ad-client="ca-pub-1482811726476217"
+        data-ad-slot="776****95"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      />
+      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+    </div>
   );
 }
